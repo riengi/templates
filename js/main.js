07934472372b7d6d55b2658c3405c4141ede43d3
riengi <112477297+riengi@users.noreply.github.com>
2023-11-09 12:23:36 +0100

    JS with JSDoc project added

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log('Hello form JS template')
+console.log(`1+1=${add(1,1)}`)
+
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns a+b
+ */
+function add(a,b) {
+    return a + b
+}
+
+
+
+
